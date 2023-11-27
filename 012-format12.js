d0ca0840ad0439da1c4234c50e427b9bcba067e5
Shelhyan <shelhyan@imac-de-shelhyan.home>
2023-11-27 12:57:59 +0100

    sevenTerre

--- 012-format12.js
+++ 012-format12.js
@@ -2,5 +2,14 @@
 //   return date.getHours() % 12 || 12;
 // }
 
-// const date = process.argv[2];
 // console.log(to12Hours(date));
+
+function to12Hours(date) {
+  return date.getHours() % 12 || 12;
+}
+let inputHours = process.argv[2];
+
+let dateInputHours = new Date();
+dateInputHours.setHours(inputHours);
+
+console.log(to12Hours(dateInputHours));
--- 013-format24.js
+++ 013-format24.js
@@ -1,7 +1,35 @@
-const date = new Date();
+// const date = new Date();
 
-console.log(
-  date.toLocaleString("fr-FR", {
-    hour12: false,
-  })
-);
+// console.log(
+//   date.toLocaleString("fr-FR", {
+//     hour12: false,
+//   })
+// );
+// function to24Hours(hours) {
+//   let date = new Date(`2000-01-01T${hours}`);
+//   let hours24 = date.getHours();
+
+//   return hours24;
+// }
+
+// let inputHours = Number(process.argv[2]);
+
+// console.log(to24Hours(inputHours));
+
+function to24Hours(hours) {
+  let [hourStr, minuteStr] = hours.split(":");
+  let hour = parseInt(hourStr, 10);
+  let minute = parseInt(minuteStr, 10);
+
+  if (hour < 12 && hours.toLowerCase().includes("pm")) {
+    hour += 12;
+  }
+
+  return (
+    hour.toString().padStart(2, "0") + ":" + minute.toString().padStart(2, "0")
+  );
+}
+
+let inputHours = process.argv[2];
+
+console.log(to24Hours(inputHours));
